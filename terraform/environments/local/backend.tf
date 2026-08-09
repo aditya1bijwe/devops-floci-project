6bdfcc65413c93faa1108b489df2ac2010ca779c
Aditya <aditya.bijwe@infosys.com>
2026-08-10 00:18:09 +0530

    feat: migrate terraform backend from local to s3+dynamodb

---
+++ terraform/environments/local/backend.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket                      = "tf-state-bucket"
+    key                         = "local/terraform.tfstate"
+    region                      = "us-east-1"
+    dynamodb_table               = "tf-locks"
+    access_key                  = "test"
+    secret_key                  = "test"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    use_path_style               = true
+
+    endpoints = {
+      s3       = "http://localhost.floci.io:4566"
+      dynamodb = "http://localhost.floci.io:4566"
+    }
+  }
+}
